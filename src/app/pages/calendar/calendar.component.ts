21fa1291c2911a7a882199f3a7226006d3ab04e1
Sneyder M <sneider.caicedo@gmail.com>
2024-11-14 11:49:38 -0500

    Calendario personalizable

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject, PLATFORM_ID, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-
 import { FullCalendarModule } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -9,6 +8,8 @@ import esLocale from '@fullcalendar/core/locales/es';
 import { CalendarOptions } from '@fullcalendar/core';
 
 import { SharedModule } from '../../shared/shared.module';
+
+import { CalendariosService } from '../../services/calendarios/calendarios.service';
 
 @Component({
   selector: 'app-calendar',
@@ -21,21 +22,53 @@ import { SharedModule } from '../../shared/shared.module';
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent {
-  calendarOptions: CalendarOptions | undefined;
+export class CalendarComponent implements OnInit {
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: 'dayGridMonth',
+    locale: esLocale,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek'
+    },
+    buttonText: {
+      today: 'Hoy',
+      month: 'Mes',
+      week: 'Semana',
+      list: 'Lista'
+    },
+    height: 'auto',
+    // events: [
+    //   { title: 'Reunión de equipo', date: '2024-11-20', backgroundColor: '#5390d9', borderColor: '#5390d9' },
+    //   { title: 'Presentación', date: '2024-11-25', backgroundColor: '#e53935', borderColor: '#e53935' }
+    // ]
+    events: []
+  };
   isBrowser: boolean;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    private calendarioService: CalendariosService
+  ) {
     this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
     if (this.isBrowser) {
-      this.calendarOptions = {
-        plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
-        initialView: 'dayGridMonth',
-        locale: esLocale,
-        events: [
-          { title: 'Prueba', date: '2024-11-01' },
-        ]
-      };
+      this.calendarioService.getEventos().subscribe((eventos) => {
+        const formattedEvents = eventos.map(evento => ({
+          title: evento.titulo,
+          start: `${evento.fecha}T${evento.hora_inicio}`,
+          end: `${evento.fecha}T${evento.hora_fin}`
+        }));
+        
+        // Asignar los eventos a calendarOptions
+        this.calendarOptions = {
+          ...this.calendarOptions,
+          events: formattedEvents
+        };
+      });
     }
   }
 }

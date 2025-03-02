885c29951e38a0c756f33267b804068e9e203e08
Gabo @Freakslab <gabcardona0782@gmail.com>
2025-03-02 17:29:02 -0500

    Update environment.prod.ts
    Se eliminan las credenciales hardcodeadas

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    HF_TOKEN: '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',
-    OPENAI_API: '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',
-    apiUrl: 'https://camaracomercio-magangue-apps.politeflower-eba2aac1.eastus2.azurecontainerapps.io',
+    HF_TOKEN: '',
+    OPENAI_API: '',
+    apiUrl: '',
 };
   

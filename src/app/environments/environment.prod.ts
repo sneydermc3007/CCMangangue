eafbf9d78c9dd427f5a1b227e82bc35bba4f8918
Gabo @Freakslab <gabcardona0782@gmail.com>
2025-03-02 21:37:43 -0500

    Update environment.prod.ts

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     HF_TOKEN: '',
     OPENAI_API: '',
-    OPENROUTER_API_KEY: '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',
+    OPENROUTER_API_KEY: '',
     apiUrl: '',
 };
   

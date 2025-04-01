37c01bdb05b68540a56473a48facf9992418a703
Sneyder M <sneider.caicedo@gmail.com>
2025-03-31 22:08:59 -0500

    Convocatorias

--- src/app/pages/convocatorias/convocatorias.component.ts
+++ src/app/pages/convocatorias/convocatorias.component.ts
@@ -25,41 +25,28 @@ export class ConvocatoriasComponent {
   public searchTerm: string = '';
   public convocatorias: any[] = [
     {
-      fecha: 'ENE 01',
-      imagen: 'https://img.freepik.com/foto-gratis/retrato-abstracto-ojo-elegancia-mujeres-jovenes-generado-ai_188544-9712.jpg',
-      titulo: '4 Claves Extorsiva',
-      descripcion: 'Camcomercio y GAULA capacitan a comerciantes contra la extorsión.'
+      fecha: 'MAR 08',
+      imagen: 'https://storageccomercio.blob.core.windows.net/fotos/mujer.png',
+      titulo: 'La Cámara de Comercio de Magangué conmemoró el Día de la Mujer con el conversatorio “Diseñadas Para Vencer”',
+      descripcion: 'La Cámara de Comercio de Magangué homenajeó a mujeres emprendedoras en un conversatorio sobre inclusión, equidad y empoderamiento.'
     },
     {
       fecha: 'ENE 01',
-      imagen: 'https://imgv3.fotor.com/images/share/fotor-ai-generate-a-lifelike-dragon.jpg',
-      titulo: 'Plan de Turismo de Magangué',
-      descripcion: 'Elaboración Público - Privada del Plan de Turismo de Magangué.'
+      imagen: 'https://storageccomercio.blob.core.windows.net/fotos/fortalecimiento.jpg',
+      titulo: 'Jornada de Fortalecimiento Empresarial en Cicuco, Bolívar',
+      descripcion: 'En Cicuco, la Cámara de Comercio de Magangué ofreció asesorías, impulso al turismo y promoción de la formalización empresarial.'
     },
     {
-      fecha: 'ENE 01',
-      imagen: 'https://marketing4ecommerce.co/wp-content/uploads/2023/10/ias-generadoras-de-imagenes.jpg',
-      titulo: 'Transformación Digital',
-      descripcion: 'Convocatoria Nacional para la Transformación de Unidades Empresariales en Centros de Transformación Digital.'
-    },
-
-    {
-      fecha: 'OCT 28',
-      imagen: 'https://www.techfunnel.com/wp-content/uploads/2018/06/what-Is-customer-identity-and-access-management.jpg',
-      titulo: 'Proveedores Internos',
-      descripcion: 'Convocatoria nuevos provedores internos en el municipio de magangué.'
+      fecha: 'FEB 11',
+      imagen: 'https://storageccomercio.blob.core.windows.net/fotos/marketing-digital.png',
+      titulo: 'Éxito en el Taller Virtual de Marketing Digital y Redes Sociales',
+      descripcion: 'El 11 de febrero, comerciantes y emprendedores participaron en un taller virtual sobre marketing digital para fortalecer sus negocios en redes sociales.'
     },
     {
-      fecha: 'NOV 22',
-      imagen: 'https://scontent.feoh1-1.fna.fbcdn.net/v/t39.30808-6/461608928_949722660485966_8789186703187262422_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=hgmNKB1kOBUQ7kNvgGg0DAw&_nc_ht=scontent.feoh1-1.fna&oh=00_AYCJ7T8ZKzJDXrXb9RpHI0QHmfy2-E79Vip7ArzgKQReGw&oe=66FD2E4E',
-      titulo: 'Zambrano inaugura su primera fábrica de tableros de triplex',
-      descripcion: 'Una de las grandes apuestas del gobierno del Bolívar Mejor es generar confianza en el territorio y garantías para que las grandes empresas puedan llegar al departamento y así ayudar al desarrollo y mejorar la economía del territorio.'
-    },
-    {
-      fecha: 'MAR 12',
-      imagen: 'https://www.rcnradio.com/_next/image?url=https%3A%2F%2Ffiles.rcnradio.com%2Fpublic%2Fstyles%2F16_9%2Fpublic%2F2023-07%2Fdinero_en_colombia_1.jpg%3FVersionId%3DPzgTrM9SqZ_Xvmte7BiJIvMsJHEiwz0R%26itok%3DNDeJqDqk&w=3840&q=75',
-      titulo: 'Gran jornada financiera para los comerciantes',
-      descripcion: 'La jornada Financiera se llevará a cabo el próximo 12 de marzo en el auditorio de la Cámara de Comercio de Magangué, con el fin de brindar a los comerciantes de la región información sobre los diferentes servicios financieros que ofrecen las entidades bancarias y financieras.'
+      fecha: 'MAR 13',
+      imagen: 'https://storageccomercio.blob.core.windows.net/fotos/cuaresma.jpg',
+      titulo: '¡Celebra la tradición en la Cuaresma de Sabores!',
+      descripcion: 'Vive la Cuaresma de Sabores en Magangué, un evento donde gastronomía, cultura y emprendimiento se unen en una experiencia única.'
     }
   ];
 
